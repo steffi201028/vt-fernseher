bb803826e7c16641e252a9ac689d4ff87b06a71b
Veronika Babic <s0554747@htw-berin.de>
2017-12-13 13:52:09 +0100

    created abstract class subject

---
+++ subject/Subject.java
@@ -0,0 +1,23 @@
+package subject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
+
+public abstract class Subject {
+
+	private final List<Observer> observerList = new ArrayList<Observer>(); 
+
+	public void register(Observer newObserver){ 
+		observerList.add(newObserver); 
+	} 
+
+	public void unregister(Observer newObserver){ 
+		observerList.remove(newObserver); 
+	} 
+
+	protected void notifyObservers(int state){ 
+		for (Observer observer : observerList) { 
+			observer.update(null, state); 
+		} 
+	} 	
+}
