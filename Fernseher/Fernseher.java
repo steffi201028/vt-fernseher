5f36289f4ecad9cc273b7099c84faa1ad40c0deb
s0550797 <s0550797@htw-berlin.de>
2017-12-13 15:48:28 +0100

    fernseher

---
+++ Fernseher/Fernseher.java
@@ -0,0 +1,29 @@
+package lulaself;
+
+public class Fernseher extends Subject { //konkretes subject, Objekt
+
+	public Fernseher() {
+		state =false;
+	    
+	}
+    private boolean state; 
+
+    public void setState(boolean state) { //an und aus schalten
+        this.state = state; 
+        //Wenn das Subject die Aktualisierung selbst durchfÃ¼hren soll, 
+        //alternativ kann die Methode auch vom Client aufgerufen werden. 
+        notifyObservers(state); 
+    } 
+
+    public boolean getState() { 
+        return state; 
+    } 
+    
+    public static void main(String args[]) {
+    ServicePoint servicepointa = new ServicePoint();
+    Fernseher fernseher = new Fernseher();
+    
+    fernseher.register(servicepointa);
+    fernseher.setState(true);
+
+} }
