4b8221bc7f96b5c889ca869a7390a97e6ecfd6fa
Veronika Babic <s0554747@htw-berin.de>
2017-12-13 17:07:31 +0100

    made it a java project!yuppie

--- src/Fernseher.java
+++ src/Fernseher.java
@@ -1,23 +1,33 @@
-import observer.ConcreteObserverA;
 
-public class Fernseher extends Subject{
 
-	private int state; 
+public class Fernseher extends Subject { //konkretes subject, Objekt
 
-    public void setState(int state) { 
+	public Fernseher() {
+		state =false;
+	    
+	}
+    private boolean state; 
+
+    public void setState(boolean state) {
+    	//an und aus schalten
         this.state = state; 
-        //Wenn das Subject die Aktualisierung selbst durchführen soll, 
+        //Wenn das Subject die Aktualisierung selbst durchfÃ¼hren soll, 
         //alternativ kann die Methode auch vom Client aufgerufen werden. 
         notifyObservers(state); 
     } 
 
-    public int getState() { 
+    public boolean getState() { 
         return state; 
-    }
+    } 
+    
+    public static void main(String args[]) {
+    	
+    	System.out.println("hallo");
+//    ServicePoint servicepointa = new ServicePoint();
+//    Fernseher fernseher = new Fernseher();
+//    
+//    fernseher.register(servicepointa);
+//    fernseher.setState(true);
+    
 
-	public void register(ConcreteObserverA newObserver) {
-		// TODO Auto-generated method stub
-		
-	} 
-
-}
+} }
